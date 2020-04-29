6f39a959a87e8d1f2f999b7d700da1441e9d4ea2
antaires <11vdlo@gmail.com>
2020-04-29 22:00:32 +0200

    setting up buffer

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 //  Copyright © 2020 Antaires. All rights reserved.
 //
 
-#include <iostream>
-
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 // window width
 const GLint WIDTH = 800, HEIGHT = 600;
@@ -19,7 +19,10 @@ const GLint WIDTH = 800, HEIGHT = 600;
 int main() {
 
     // GLFW init
-    glfwInit();
+    if (!glfwInit()){
+        return -1;
+    }
+    
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -40,18 +43,38 @@ int main() {
     
     // GLEW init
     glewExperimental = GL_TRUE;
-    if( GLEW_OK != glewInit()){
+    if( glewInit() != GLEW_OK){
         std::cout<<"Failed to initialize GLEW"<<std::endl;
         return -1;
     }
+    std::cout<<"\nVersion: "<<glGetString(GL_VERSION)<<std::endl;
     glViewport(0, 0, screenWidth, screenHeight);
+    
+    float positions[6] = {
+      -0.5f, -0.5f,
+       0.5f,  0.5f,
+       0.5f, -0.5f,
+    };
+    
+    // define a buffer
+    unsigned int buffer;
+    glGenBuffers(1, &buffer ); // gives us back an id
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind the buffer (bound is the one drawn)
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); // put data in buffer
+    
+    // Vertex Attributes and Layouts in OpenGL 6:14
     
     // GAME LOOP
     while (!glfwWindowShouldClose(window)){
-        glfwPollEvents();
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 1 is fully visible alpha
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 1 is fully visible alpha
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
         glfwSwapBuffers(window);
+        
+        glfwPollEvents();
+
     }
     
     // CLEAN UP
