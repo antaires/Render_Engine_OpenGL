26566e80f4bd1fcc1e3c5496659d25c7753c7b2f
antaires <11vdlo@gmail.com>
2020-06-01 09:05:14 +0200

    add basic model view projection matrices where view simulates a camera

--- main.cpp
+++ main.cpp
@@ -97,14 +97,19 @@ int main() {
         
         // create projection matrix
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); // orthographic matrix 4:3
+        // view matrix (position of our camera - no camera actually, just move world opp direction)
+        // can automate with camera class
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0, 0));
+        // model matrix
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
         
-        
+        glm::mat4 mvp = proj * view * model; // reverse order due to openGL column order
         
         // create shader
         Shader shader("/Users/valiaodonnell/Documents/openGL/Experiments_in_OpenGL/Experiments_in_OpenGL/res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
         
         // texture
         Texture texture("/Users/valiaodonnell/Documents/openGL/Experiments_in_OpenGL/Experiments_in_OpenGL/res/textures/testTexture.png");
